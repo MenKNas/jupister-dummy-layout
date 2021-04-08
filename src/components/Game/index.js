c0707f323f8daad4cb5d2ed932b27a018c5eb40f
Menios Nas <men.nasies@gmail.com>
2021-04-08 21:35:08 +0300

    refactor game component

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -3,15 +3,11 @@ import MainButton from "../Buttons/MainButton";
 import useWindowSize from "../../hooks/useWindowSize";
 
 export const Game = React.memo(({ gameTitle, gameImg, className }) => {
-  const [opacity, setOpacity] = React.useState({ main: 1, overlay: 0 });
-
   const { width } = useWindowSize();
   return (
     <div
       className={`block rounded-md relative overflow-hidden ${className}`}
       data-component="Game"
-      onMouseEnter={() => setOpacity({ main: 1, overlay: 1 })}
-      onMouseLeave={() => setOpacity({ main: 1, overlay: 0 })}
       style={{
         backgroundImage: `url(${gameImg})`,
         backgroundPosition: "center center",
@@ -22,21 +18,14 @@ export const Game = React.memo(({ gameTitle, gameImg, className }) => {
         maxHeight: "100%",
         maxWidth: "100%",
         paddingTop: "67%",
-        opacity: opacity.main,
+        opacity: 1,
         transition: "opacity 0.25s ease-in 0s",
         backgroundRepeat: "no-repeat",
       }}
     >
       {
         width > 1024 ? (
-          <div
-            className="absolute top-0 left-0 bottom-0 right-0 transform-gpu text-center transition duration-500 ease-in-out flex flex-col items-center justify-evenly"
-            style={
-              opacity.overlay === 1
-                ? { background: "rgba(0, 0, 0, 0.5)", opacity: 1 }
-                : { background: "none", opacity: 0 }
-            }
-          >
+          <div className="absolute top-0 left-0 bottom-0 right-0 transform-gpu text-center transition duration-500 ease-in-out flex flex-col items-center justify-evenly opacity-0 hover:opacity-100 hover:bg-gray-500 hover:bg-opacity-50">
             <span className="text-white"> {gameTitle} </span>
             <MainButton secondary> Try it</MainButton>
             <MainButton> Demo </MainButton>
